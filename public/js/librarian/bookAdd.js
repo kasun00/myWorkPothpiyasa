0fd9f1bf56e16dfe6bfbe3892911cd7f716c6498
kasun00 <92900452+kasun00@users.noreply.github.com>
2022-12-21 10:05:38 +0530

    Add files via upload

--- public/js/librarian/bookAdd.js
+++ public/js/librarian/bookAdd.js
@@ -5,10 +5,12 @@ let vendor = document.getElementById('vendor');
 let donor = document.getElementById('donor');
 let addbtn = document.getElementById('addbookbtn');
 let addpopup = document.getElementById('container_popup');
+// let defaultTask = document.getElementById('defaultTask');
 
 function getVendor() {
     vendor.style.visibility='visible';
     donor.style.visibility = 'hidden';
+    // defaultTask.style.visibility = 'hidden';
     btn.style.left = "0px";
     donorBtn.style.color = "black";
     vendorBtn.style.color = "white";
@@ -19,6 +21,7 @@ function getVendor() {
 function getDonor(){
     vendor.style.visibility = "hidden";
     donor.style.visibility = "visible";
+    // defaultTask.style.visibility = 'hidden';
     btn.style.left = "125px";
     vendorBtn.style.color = "black";
     donorBtn.style.color = "white";
